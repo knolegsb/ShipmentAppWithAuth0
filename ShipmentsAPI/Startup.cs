6e13587bccf3cb9ec733becb0903a028dbe803b4
knolegsb <seon2you@hotmail.com>
2015-11-10 13:41:31 -0800

    added startup and webapiconfig

---
+++ ShipmentsAPI/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using ShipmentsAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace ShipmentsAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Regtister(config);
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+    }
+}
