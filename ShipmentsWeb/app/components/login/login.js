6fe2c1216a989bc5cd09b0494174de2714a657a5
knolegsb <seon2you@hotmail.com>
2015-11-10 20:16:47 -0800

    completed login

--- ShipmentsWeb/app/components/login/login.js
+++ ShipmentsWeb/app/components/login/login.js
@@ -1,8 +1,12 @@
 ﻿angular.module('shipments.login', [
     'auth0'
 ])
-.controller('LoginCtrl', function ($scope) {
+.controller('LoginCtrl', function ($scope, auth, $location) {
     $scope.login = function () {
-
+        auth.signin({}, function (profile, token) {
+            $location.path('/');
+        }, function (error) {
+            console.log("There was an error", error);
+        });
     };
 });
--- ShipmentsWeb/app/config.js
+++ ShipmentsWeb/app/config.js
@@ -1,3 +1,3 @@
-﻿var AUTH0_DOMAIN = 'your_domain';
-var AUTH0_CLIENT_ID = 'your_client_id';
-var SERVICE_BASE = 'http://localhost:36740';
+﻿var AUTH0_DOMAIN = 'soileep.auth0.com';
+var AUTH0_CLIENT_ID = 'RZzuy0MIgnHRFnQE9fUPRLrBYqkNn5ym';
+var SERVICE_BASE = 'http://localhost:46931';
